49981b0078cf383d367b17456ca6de85e41b2beb
hankim <42214405+byhankim@users.noreply.github.com>
2021-12-10 21:48:52 +0900

    Update navigation codelab test

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.compose.rally
 
 import android.os.Bundle
@@ -93,13 +77,15 @@ fun RallyNavHost(navController: NavHostController, modifier: Modifier = Modifier
                 onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
                 onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
                 onAccountClick = { name ->
-                    navigateToSingleAccount(navController, name)
+//                    navigateToSingleAccount(navController, name)
+                    navController.navigate("${RallyScreen.Accounts.name}/$name")
                 },
             )
         }
         composable(RallyScreen.Accounts.name) {
             AccountsBody(accounts = UserData.accounts) { name ->
-                navigateToSingleAccount(navController = navController, accountName = name)
+//                navigateToSingleAccount(navController = navController, accountName = name)
+                navController.navigate("Accounts/${name}")
             }
         }
 
@@ -116,7 +102,7 @@ fun RallyNavHost(navController: NavHostController, modifier: Modifier = Modifier
             ),
             deepLinks = listOf(
                 navDeepLink {
-                    uriPattern = "rally://$accountsName/{name}"
+                    uriPattern = "example://rally/$accountsName/{name}"
                 }
             ),
         ) { entry ->
